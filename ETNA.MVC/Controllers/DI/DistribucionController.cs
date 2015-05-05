d2268264f041954db53b9cfacaed06ff411ec6ca
martinmanrique18 <martinmanrique18@gmail.com>
2015-05-05 11:03:28 -0500

    Menu Distribucion

---
+++ ETNA.MVC/Controllers/DI/DistribucionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ETNA.MVC.Controllers.DI
+{
+    public class DistribucionController : Controller
+    {
+        //
+        // GET: /Distribucion/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ETNA.MVC/Controllers/DI/EntregaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ETNA.MVC.Controllers.DI
+{
+    public class EntregaController : Controller
+    {
+        //
+        // GET: /Entrega/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
